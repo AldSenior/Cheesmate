168800d28ecf1607fe67f57487a28591d991f07f
AldSenior <aldsio196@gmail.com>
2025-03-19 19:58:57 +0300

    develep

--- chees-project/vite.config.ts
+++ chees-project/vite.config.ts
@@ -5,4 +5,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	base: '/',
+	server: {
+		host: '0.0.0.0', // Слушаем на всех интерфейсах
+		port: 5173, // Порт, который использует Vite
+		allowedHosts: [
+			'cheesmate-web.onrender.com', // Разрешаем запросы с этого домена
+		],
+	},
 })
